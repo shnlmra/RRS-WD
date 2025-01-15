e233fbe9dfccdffd4d11a5e15c9cd494500f6a28
itsmiyukimharie <miyukimhariecparocha@iskolarngbayan.pup.edu.ph>
2025-01-15 09:08:22 +0800

    UI - Audit Logs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DarbenDbConnection")));
 
 var app = builder.Build();
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=AuditLogs}/{action=Index}/{id?}");
 
 app.Run();
