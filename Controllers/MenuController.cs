f1c307608278574d1b58eefafa491e708d93b17d
codebeen <darbenvlamonte@iskolarngbayan.pup.edu.ph>
2025-01-16 00:36:55 +0800

    unfinished menu management

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RRS.Data;
+
+namespace RRS.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public MenuController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var menu = context.Menus.ToList();
+            return View(menu);
+        }
+    }
+}
---
+++ Models/DataTransferObject/MenuDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RRS.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace RRS.Models.DataTransferObject
+{
+    public class MenuDto
+    {
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string? Description { get; set; }
+        [Required, Precision(10, 2)]
+        public decimal Price { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Foreign key
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
