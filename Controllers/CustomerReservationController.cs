8b3ca66d273e2f99c80bc20d91583498117067e1
Shanella Cagulang <141259116+shnlmra@users.noreply.github.com>
2025-01-23 23:52:54 +0800

    add connection

---
+++ Controllers/CustomerReservationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RRS.Controllers
+{
+    public class CustomerReservationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DarbenDbConnection2")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ShanellaConnection")));
 
 var app = builder.Build();
 
