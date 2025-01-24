408dcda6e8273b485edd23bc5c0cc52e360e6538
shnlmra <141259116+shnlmra@users.noreply.github.com>
2025-01-25 02:50:30 +0800

    Merge-UI-Customer

---
+++ Controllers/CustomerLoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using System.Linq;
+
+namespace RRS.Controllers
+{
+    public class CustomerLoginController : Controller
+    {
+        // Set the constant for cookie authentication scheme
+        private const string CookieAuthenticationDefaults = "Cookies";
+
+        // Returns the CustomerLogin view
+        public IActionResult CustomerLogin()
+        {
+            return View("~/Views/Account/CustomerLogin.cshtml");
+        }
+    }
+}
