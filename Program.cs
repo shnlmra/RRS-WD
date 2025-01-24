8247ce74469994770d53869a036b02f76f9edd5e
shnlmra <141259116+shnlmra@users.noreply.github.com>
2025-01-24 22:28:50 +0800

    Pending

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ app.UseAuthorization();
 // Configure routes properly
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Reservation}/{action=Create}/{id?}");
+    pattern: "{controller=reservation}/{action=Create}/{id?}");
 
 // Uncomment this if you want a specific route for the AboutUs controller
 //app.MapControllerRoute(
